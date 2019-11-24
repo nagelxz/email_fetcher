5fb9aea01303664d613b185bb63763f3ae9199db
Jon Nagel <nagelxz@users.noreply.github.com>
2019-11-24 16:59:00 -0500

    Update mail_fetcher.py

--- mail_fetcher.py
+++ mail_fetcher.py
@@ -5,6 +5,9 @@ import json
 import actions
 
 
+email_user = os.environ.get('email_username')
+email_pass = os.environ.get('email_password')
+server = os.environ.get('email_server')
 search = json.load(open('search_actions.json','r'))
 
 print(search['email'])
@@ -16,8 +19,8 @@ def process_message(mail,action):
 
 #connect and login to the mailserver
 try:
-    mailserver = IMAP4_SSL(environ['server'])
-    resp = mailserver.login(environ['user'],environ['passwd'])
+    mailserver = IMAP4_SSL(server)
+    resp = mailserver.login(email_user,email_pass)
 except imaplib.error as err:
     print(err)
     print("unable to login or connect to server.")
